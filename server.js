acb8c8b18e239ece5bafbab25625b491300f7de0
Rogerio Gonzaga <rogeriogonzaga84@gmail.com>
2026-09-18 23:22:36 -0300

    Editado novo arquivo server HTTP

---
+++ server.js
@@ -0,0 +1,17 @@
+// Subindo um server HTT em nodeJS
+
+const http = require("http")
+const host = "127.0.0.1"
+const port = 3000
+
+const server = http.createServer((req , res)=> {
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'text\plain');
+    res.end('Ola mundo \n meu primeiro script!!!')
+
+})
+
+server.listen(port, host, ()=> {
+    console.log(`Server running at http://${host}:${port}`)
+    
+})
